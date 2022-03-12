c17a14fecb0925e30d98f46424bb2febb0a22bc5
4yn <4yn@users.noreply.github.com>
2022-03-12 15:02:01 +0800

    keys ui

--- src/WordleHistory.js
+++ src/WordleHistory.js
@@ -1,5 +1,34 @@
+import './WordleHistory.css'
+
+const WordleRow = ({guess}) => {
+  return <div className="flex-horizontal flex-space">
+    {guess.map((guess, idx) => (
+      <div className={`flex-center flex-grow wordle-char ${guess[1]}`}>
+        {guess[0]}
+      </div>
+    ))}
+  </div>
+}
+
+const NewRound = () => {
+  return <div className="flex-center new-round">
+    NEW ROUND
+  </div>
+}
+
 const WordleHistory = () => {
-  return <></>
+  return <div className="flex-vertical wordle-history">
+    <WordleRow guess={
+      [
+        ["C", "yellow"],
+        ["R", "green"],
+        ["A", "black"],
+        ["N", "black"],
+        ["E", "black"]
+      ]
+    }/>
+    <NewRound/>
+  </div>
 }
 
 export default WordleHistory 
--- src/WordleKeyboard.js
+++ src/WordleKeyboard.js
@@ -1,5 +1,37 @@
-const WordleKeyboard = () => {
-  return <></>
+import "./WordleKeyboard.css"
+
+import { useMemo, useCallback } from "react"
+
+const keys = [
+  ["Q","W","E","R","T","Y","U","I","O","P"],
+  ["A","S","D","F","G","H","J","K","L"],
+  ["✔️","Z","X","V","B","N","M","❌"]
+]
+
+const defaultKeyState = {
+  "Y": "yellow",
+  "G": "green",
+  "B": "black",
+}
+
+const WordleKeyboard = ({keyState = defaultKeyState, onKey = () => {}}) => {
+  return <div className="flex-vertical flex-space full-height">
+    {
+      keys.map((row, idx) => (
+        <div className="flex-horizontal flex-space flex-grow" key={idx}>
+          {
+            row.map((key, idx) => (
+              <div className={`wordle-key flex-grow flex-center ${
+                keyState[key]
+              }`} key={idx} onClick={() => {onKey(key)}}>
+                {key}
+              </div>
+            ))
+          }
+        </div>
+      ))
+    }
+  </div>
 }
 
 export default WordleKeyboard 
